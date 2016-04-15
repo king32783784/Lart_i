152bfe7a8079e058580ea427a1e71da37b0a7607
peng.li <king32783784@163.com>
2016-04-15 22:50:18 +0800

    Perfect TestParpare ()

--- server/client/preparetest.py
+++ server/client/preparetest.py
@@ -2,6 +2,8 @@
     Enable client-side test preparation
 '''
 import os 
+import subprocess
+from subprocess import PIPE, Popen
 
 
 class TestParpare():
@@ -11,20 +13,35 @@ class TestParpare():
         '''
         pass
 
-    def baseddependency(self):
+    def baseddependency(self, *args):
         '''
            Check test tool based on
         '''
-        pass
+        deletioncmdlist = []
+        for arg in args:
+            testcmd = Popen('which %s' % arg, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            testcmd.wait()
+            exitcode = testcmd.poll()
+            if exitcode != 0:
+                deletioncmdlist.append(arg)
+        return deletioncmdlist
 
-    def mkdirectory(self):
-        pass
-
-    def resultsdirectory(self):
+    def joinpath(fun):
+        def add_mkdir(self, default, sep, *args):
+            try:
+                default = os.path.join(default, sep.join(args))
+                os.makedirs(default)
+            except OSError:
+                pass  
+            return fun(self, default, sep, *args)
+        return add_mkdir
+ 
+    @joinpath
+    def mkdirectory(self, default, sep, *args):
         '''
         Test results directory processing
         '''
-        pass
+        return default
 
     def logdirectory(self):
         '''
@@ -43,3 +60,9 @@ class TestParpare():
         Test tool storage directory
         '''
         pass
+# testcase
+#a = TestParpare()
+#b=a.mkdirectory('testresult/dafault', '/', 'performance', 'Perf_cpu', 'result')
+#print b
+#print a.baseddependency('make', 'gcc', 'g++', 'java', 'hello', 'ls')
+
